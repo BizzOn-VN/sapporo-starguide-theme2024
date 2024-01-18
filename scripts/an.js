dbd912085e80f3c3a0a4eb5f4c7a71d3f5df3974
an0209vn <48006180+an0209vn@users.noreply.github.com>
2024-01-18 22:26:50 +0700

    update html

--- scripts/an.js
+++ scripts/an.js
@@ -191,9 +191,72 @@ set_max_height_scrollbar();
 
 
 
+$('.blk-history-2 .slider-desktop').owlCarousel({
+    margin:0,
+    loop:true,
+    smartSpeed: 1800,
+    autoplay:true,
+    autoplayTimeout:7000,
+    autoplayTimeout:17000,
+    // autoplayHoverPause:true,
+    // autoplayHoverPause: true ,
+    autoWidth:true,
+    items:4
+})
 
+$( document ).ready(function() {
+		
+    	
+    	
+});
 
+$('.blk-history-2 .slider-mb').owlCarousel({
+    margin:0,
+    loop:true,
+    smartSpeed: 1800,
+    autoplay:true,
+    autoplayTimeout:7000,
+    autoplayTimeout:17000,
+    // autoplayHoverPause:true,
+    // autoplayHoverPause: true ,
+    autoWidth:true,
+    items:4
+})
 
+	function set_top_nav_introduce_slider(){
+		$(".blk-history-4 .slider-introduce .item .produce").css("height","auto")
+		$(".blk-history-4 .owl-item").each(function() {
+			if(maxHeight<$(this).children(".item").children(".produce").height()){
+				maxHeight=$(this).children(".item").children(".produce").height();
+				console.log(maxHeight)
+			}
+  			// maxHeight = maxHeight < $(this).height() ? maxHeight :     $(this).height();
+  			$(".blk-history-4 .slider-introduce .item .produce").css("height",maxHeight)
+  			$(".blk-history-4 .slider-introduce .owl-nav .owl-prev").css('top',maxHeight/2);
+  			$(".blk-history-4 .slider-introduce .owl-nav .owl-next").css('top',maxHeight/2);
+		});
+		
+	}
+
+var owl_introduce =$('.blk-history-4 .slider-introduce').owlCarousel({
+    loop: true,
+	nav: true,
+	dots: false,
+	smartSpeed: 800,
+	touchDrag: false,
+	mouseDrag: false,
+	pullDrag: false,
+	items: 1,
+	onInitialized: set_top_nav_introduce_slider(),
+    onResized: set_top_nav_introduce_slider(),
+    onTranslated: set_top_nav_introduce_slider()
+})
+
+owl_introduce.on('changed.owl.carousel', function(e) {
+  var maxHeight = -1;
+		    
+
+});
 $('.info-gift .item').hover(function(){
 		$(".info-gift .item .info").removeClass("active");
         $(this).children(".info").addClass("active");
