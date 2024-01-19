cc4b45e6d9d2ea2ee2e4c50af70ddde57404a4ea
an0209vn <48006180+an0209vn@users.noreply.github.com>
2024-01-19 22:17:07 +0700

    update html

--- scripts/an.js
+++ scripts/an.js
@@ -60,7 +60,23 @@ if (window.matchMedia("(max-width: 768px)").matches) {
 });
 } else {
 }
-
+$('.banner').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dot:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 $('.s-days-slider').owlCarousel({
     loop:true,
     margin:0,
